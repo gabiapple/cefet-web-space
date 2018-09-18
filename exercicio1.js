408b5572b5a4b2f7dd9c65a4c1e037dde687a408
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2018-09-18 12:02:17 -0300

    Exercicio1 done

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,19 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let buttons = document.querySelectorAll('.botao-expandir-retrair');
+
+for(let button of buttons){
+    button.addEventListener('click', function(e) {
+        alert('clicou no botao mais');
+        let no = e.currentTarget;
+        let parent = no.parentNode;
+        parent.classList.toggle('expandido');
+        console.log(no.innerHTML);
+        no.innerHTML = no.innerHTML == '+' ? '-' : '+';
+        console.log(no.innerHTML);
+    });
+    
+}
+
+
