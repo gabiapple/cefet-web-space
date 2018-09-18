f37a93a0973f2d6ea2c1065093fbe0f11727a4c4
Gabriela Ramalho <gabrielaramalho96@gmail.com>
2018-09-18 12:22:16 -0300

    Exercicio2 done

--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,22 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+const changeImage = function(factor){
+    cont = (cont == 0 && factor < 0) ? 4 : cont + factor;
+    image = document.querySelector('#slide');
+    image.src = servidorDasImagens + todasAsImagens[cont % 5];
+}
+
+let cont = 0;
+
+let buttonPrev = document.querySelector('#anterior');
+let buttonNext = document.querySelector('#proximo');
+
+buttonPrev.addEventListener('click', function(e){
+    changeImage(-1);
+});
+
+buttonNext.addEventListener('click', function(e){
+     changeImage(1);
+});
